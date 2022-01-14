d1bc244cdfdf43a7a0618e773d9685a35ad81178
DmitriyFedan <dmitry061093@mail.ru>
2022-01-14 22:29:06 +0700

    add Jsondeserializer

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace TESTworkElcomplus.Models
+namespace testwork
 {
-    internal class Class1
+    public class JsToArrDeserializer
     {
+        async public Task<string[]> StrArrayReturn(string filePath)
+        {
+
+            if (File.Exists(filePath))  
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+                {
+                    //List<string> st = await JsonSerializer.DeserializeAsync<List<string>>(fs);
+                    //Dictionary<string, string[]> st = await JsonSerializer.DeserializeAsync<Dictionary<string, string[]>>(fs);
+
+                    return await Task.Run(() => st["Values"]);
+                }
+            }
+            return null;
+        }
     }
 }
