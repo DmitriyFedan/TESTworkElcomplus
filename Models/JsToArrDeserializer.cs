ced31c333ea40b257f70b600b7a40d8d34b5a4d0
DmitriyFedan <dmitry061093@mail.ru>
2022-01-14 22:44:05 +0700

    add interface and class xmldeser

--- Models/JsToArrDeserializer.cs
+++ Models/JsToArrDeserializer.cs
@@ -8,17 +8,17 @@ using System.Threading.Tasks;
 
 namespace testwork
 {
-    public class JsToArrDeserializer
+    public class JsToArrDeserializer : IDeserializer
     {
         async public Task<string[]> StrArrayReturn(string filePath)
         {
 
-            if (File.Exists(filePath))  
+            if (File.Exists(filePath))  //  проверка на наличие не нужна, т.к  будем передавать файл. Убрать позже
             {
                 using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
                 {
                     //List<string> st = await JsonSerializer.DeserializeAsync<List<string>>(fs);
-                    //Dictionary<string, string[]> st = await JsonSerializer.DeserializeAsync<Dictionary<string, string[]>>(fs);
+                    Dictionary<string, string[]> st = await JsonSerializer.DeserializeAsync<Dictionary<string, string[]>>(fs);
 
                     return await Task.Run(() => st["Values"]);
                 }
